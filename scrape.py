ec451fd1ae6eca1790e9a0707db1afcc9f3764cd
Sanjay S <srikumar.sanjay@gmail.com>
2021-01-25 14:08:13 -0500

    changed how logs are made

--- scrape.py
+++ scrape.py
@@ -5,41 +5,48 @@ from bs4 import BeautifulSoup
 import requests
 import twilio
 from twilio.rest import Client
+import pickle
 
 def get_open_sections(url):
     r = requests.get(url).text
-    soup = bs4.BeautifulSoup(r, 'html.parser')
+    soup = bs4.BeautifulSoup(r, "html.parser")
     instructor_name = soup.findAll("span", {"class": "section-instructor"})
     section_id = soup.findAll("span", {"class": "section-id"})
     open_seats = soup.findAll("span", {"class": "open-seats-count"})
     return zip(instructor_name, section_id, open_seats)
 
-def text(body ):
-    account_sid = "#"
-    auth_token = "#"
+
+def text(body):
+    account_sid = "ACef4e810916850ca7e12a626e3aa23da8"
+    auth_token = "e7f7bb7c77c78a66de0fa9c9a85c2e4a"
     client = Client(account_sid, auth_token)
-    client.messages.create(
-    to="#",
-    from_="#",
-    body=body)
+    client.messages.create(to="17322087157", from_="17609144209", body=body)
 
 
-if __name__ == '__main__':
-    #{"Stat":'https://app.testudo.umd.edu/soc/search?courseId=stat400&sectionId=&termId=202008&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on'
-    classes = { 
-    "Math240": 'https://app.testudo.umd.edu/soc/search?courseId=MATH240&sectionId=&termId=202008&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on'
+if __name__ == "__main__":
+    classes = {
+        "CMSC351":
+       "https://app.testudo.umd.edu/soc/search?courseId=CMSC351&sectionId=&termId=202101&_openSectionsOnly=on&creditCompare=%3E%3D&credits=0.0&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on",
+       "MATH241":
+       "https://app.testudo.umd.edu/soc/search?courseId=Math241&sectionId=&termId=202101&_openSectionsOnly=on&creditCompare=%3E%3D&credits=0.0&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on"
     }
-    message = ''
+    message = ""
+    professors = ["Justin Wyss-Gallifent", "Terence Long"]
     for course in classes:
         check = get_open_sections(classes[course])
         for name, c_id, seats in check:
             name = name.text.strip()
-            c_id =c_id.text.strip()
+            c_id = c_id.text.strip()
             seats = seats.text.strip()
-            #print(name, c_id, seats)
-            if name == "Eoin Mackall" or name =="Allan Yashinski":
-                if int(seats) > 0 :
-                    message += f'SECTION {c_id} PROF: {name}; {seats} SEATS OPEN!!! \n'
-    if message:
+            if name in professors:
+                if int(seats) > 0:
+                    message += f"{course}; section {c_id} with {name}: {seats} seat\n"
+    try:
+        log = pickle.load(open("log.p", "rb"))
+    except:
+        log = None
+    if message and log != message:
         text(message)
-            
+    pickle.dump(message, open("log.p", "wb"))
+
+  
